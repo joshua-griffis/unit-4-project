a0ff8424c0567b942edf2e4be5734931f0c0b85c
Joshua <joshuagiff03@email.com>
2022-10-21 08:26:08 -0500

    today

--- app/models.py
+++ app/models.py
@@ -9,7 +9,14 @@ class Item(models.Model):
     description = models.TextField()
     image = models.TextField()
     price = models.IntegerField()
+    cart = models.BooleanField()
 
 
 def create_item(nam, desc, img, pri):
-    return Item(name=nam, description=desc, image=img, price=pri).save()
+    return Item(name=nam, description=desc, image=img, price=pri, cart=False).save()
+
+
+def add_to_cart(nam):
+    item = Item.objects.get(name=nam)
+    item.cart = True
+    return item.save()
